5f1e76690f0d0d65203656f7c543ea60a697a535
Ben O'Hara <bpo@robotparade.co.uk>
2012-09-26 16:45:36 +0100

    Initial project setup.

---
+++ src/elm.shell/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * User: 721116
+ * Date: 26/09/2012
+ * Time: 16:36
+ * 
+ *  */
+using System;
+
+namespace elm.shell
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
