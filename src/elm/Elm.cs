b1756d0de04c82b228239f3101ed0107440e4dc0
Ben O'Hara <bpo@robotparade.co.uk>
2012-10-11 09:05:14 +0100

    Added basic logging & (prematurely) added Windsor wraps.

--- src/elm/Elm.cs
+++ src/elm/Elm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Castle.Core.Logging;
 using elm.Extensions;
 using elm.Web;
 using OpenFileSystem.IO;
@@ -8,6 +9,12 @@ namespace elm
 	public class Elm
 	{
 		private readonly ISharepointService sharepointService;
+		private ILogger log=NullLogger.Instance;
+		
+		public ILogger Log {
+			get { return log; }
+			set { log = value; }
+		}
 		
 		public Elm(ISharepointService sharepointService)
 		{
@@ -16,13 +23,15 @@ namespace elm
 		
 		public ElmResponse Upload(IFile source, Uri destination,string checkInComments)
 		{
+			log.DebugFormat("Upload of '{0}' to '{1}' beginning.",source.Name,destination.AbsoluteUri);
 			ElmResponse r=new ElmResponse();
 			try{
 				sharepointService.CheckOutFile(destination,false,DateTime.Now);
 				sharepointService.UploadFile(destination,source.ToByteArray());
 				sharepointService.CheckInFile(destination,checkInComments,CheckInType.CheckinMajorVersion);
 				r.Status=Status.Ok;
-			}catch(Exception){
+			}catch(Exception ex){
+				log.ErrorFormat(ex,"Error uploading File.");
 				throw new ElmException();
 			}
 			
